6919c28b5d66f214cca39484084fcedfdb6db9d3
Raj <singhayush88805@gmail.com>
2024-09-10 22:41:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -159,7 +159,7 @@ async def start_command(client: Client, message: Message):
                 TUT_VID = f"https://t.me/ultroid_official/18"
                 token = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
                 await update_verify_status(id, verify_token=token, link="")
-                link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API,f'https://telegram.dog/{client.username}?start=verify_{token}')
+                link = await get_shortlink(SHORTLINK_URL, SHORTLINK_API, f'https://telegram.dog/{client.username}?start=verify_{token}')
                 btn = [
                     [InlineKeyboardButton("Click here", url=link)],
                     [InlineKeyboardButton('How to use the bot', url=TUT_VID)]
